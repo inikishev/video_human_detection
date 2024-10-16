6244df6c65faa527f1892c9a5093a95599433de5
qq-me <nkshv2@gmail.com>
2024-10-16 12:22:05 +0300

    add opencv renderer

---
+++ video_human_detection/render.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+
+
+class OpenCVRenderer:
+    """A frame by frame video renderer using OpenCV.
+
+    Example:
+    ```
+    # create a renderer
+    renderer = OpenCVRenderer('out.mp4', fps = 60)
+
+    # add 1000 640x320 frames with random values
+    for i in range(1000):
+        frame = np.random.uniform(0, 255, size = (640, 320, 3)
+        renderer.add_frame(frame)
+
+    # release the renderer, which makes the video file openable
+    renderer.release()
+    ```
+    """
+
+    def __init__(
+        self,
+        outfile,
+        fps,
+        codec="mp4v",
+    ):
+        """Create a video renderer.
+
+        :param outfile: Path to the file to write the video to. The file will be created when first frame is added.
+        :param fps: Frames per second.
+        :param codec: Video codec, default is 'mp4v'
+        """
+        if not outfile.lower().endswith(".mp4"):
+            outfile += ".mp4"
+        self.outfile = outfile
+        self.fps = fps
+        self.codec = codec
+        # self.writer = cv2.VideoWriter(outfile, cv2.VideoWriter_fourcc(*codec), fps, self.shape)
+        self.writer = None
+
+    def add_frame(self, frame: np.ndarray):
+        """Write the next frame to the video file. All frames must of the same shape, and have (H, W, 3) format.
+
+        :param frame: Frame in (H, W, 3) format.
+        :raises ValueError: If frame shape is different from the previous one.
+        """
+        # on first frame create writer and use frame shape as video size
+        if self.writer is None:
+            self.shape = frame.shape
+            self.writer = cv2.VideoWriter(
+                self.outfile, cv2.VideoWriter_fourcc(*self.codec), self.fps, self.shape
+            )
+
+        # check frame shape (opencv doesn't have that check, it just skips the frame)
+        if frame.ndim != 3:
+            raise ValueError(
+                f"Frame must have 3 dimensions: (H, W, 3), got frame of shape {frame.shape}"
+            )
+        if frame.shape[2] != 3:
+            raise ValueError(
+                f"The last frame dimension must be 3 (RGB), got frame of shape {frame.shape}"
+            )
+        if frame.shape != self.shape:
+            raise ValueError(
+                f"Frame size {frame.shape} is different from previous frame size {self.shape}"
+            )
+
+        # write the frame to file.
+        self.writer.write(frame)
+
+    def release(self):
+        """Close the writer, releasing access to the video file."""
+        if self.writer is None:
+            raise ValueError("No frames have been added to this renderer.")
+        self.writer.release()
