5d7e65402235fef9b50bf3411605067804645add
qq-me <nkshv2@gmail.com>
2024-10-16 13:47:55 +0300

    set to instance coloring

--- video_human_detection/render.py
+++ video_human_detection/render.py
@@ -12,7 +12,7 @@ class OpenCVRenderer:
 
     # add 1000 640x320 frames with random values
     for i in range(1000):
-        frame = np.random.uniform(0, 255, size = (640, 320, 3)
+        frame = np.random.uniform(0, 255, size = (640, 320, 3))
         renderer.add_frame(frame)
 
     # release the renderer, which makes the video file openable
@@ -32,25 +32,29 @@ class OpenCVRenderer:
         :param fps: Frames per second.
         :param codec: Video codec, default is 'mp4v'
         """
+        if fps < 1: raise ValueError(f"FPS must be at least 1, got {fps}")
         if not outfile.lower().endswith(".mp4"):
             outfile += ".mp4"
+
         self.outfile = outfile
         self.fps = fps
         self.codec = codec
-        # self.writer = cv2.VideoWriter(outfile, cv2.VideoWriter_fourcc(*codec), fps, self.shape)
+
         self.writer = None
 
     def add_frame(self, frame: np.ndarray):
-        """Write the next frame to the video file. All frames must of the same shape, and have (H, W, 3) format.
+        """Write the next frame to the video file.
+        All frames must of the same shape, have (H, W, 3) format and np.uint8 data type.
 
-        :param frame: Frame in (H, W, 3) format.
+        :param frame: Frame in (H, W, 3) format and np.uint8 data type.
         :raises ValueError: If frame shape is different from the previous one.
         """
         # on first frame create writer and use frame shape as video size
         if self.writer is None:
             self.shape = frame.shape
+
             self.writer = cv2.VideoWriter(
-                self.outfile, cv2.VideoWriter_fourcc(*self.codec), self.fps, self.shape
+                self.outfile, cv2.VideoWriter_fourcc(*self.codec), self.fps, (self.shape[1], self.shape[0])
             )
 
         # check frame shape (opencv doesn't have that check, it just skips the frame)
@@ -66,8 +70,10 @@ class OpenCVRenderer:
             raise ValueError(
                 f"Frame size {frame.shape} is different from previous frame size {self.shape}"
             )
+        if frame.dtype != np.uint8:
+            raise ValueError(f"Frame must be of type np.uint8, got {frame.dtype}")
 
-        # write the frame to file.
+        # write new frame to file
         self.writer.write(frame)
 
     def release(self):
